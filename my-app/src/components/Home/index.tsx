693ab39932d24351a4f2ec18c13feb246d71438e
novakvova <novakvova@gmail.com>
2022-02-19 10:24:08 +0200

    Upload image

--- my-app/src/components/Home/index.tsx
+++ my-app/src/components/Home/index.tsx
@@ -1,80 +1,31 @@
-import "cropperjs/dist/cropper.min.css";
 import * as React from 'react';
-import { Modal, Button, Row, Col } from 'antd';
-import Cropper from "cropperjs";
+import CropperModal from '../common/CropperModal';
+import { Col, Row } from "antd";
 
 const HomePage : React.FC = () => {
-    const imgRef = React.useRef<HTMLImageElement>(null);
-    const prevRef = React.useRef<HTMLImageElement>(null);
-    const [cropperObj, setCropperObj] = React.useState<Cropper>();
 
-    const [visible, setVisible] = React.useState<boolean>(false);
-    const [imageView, setImageView] = React.useState<string>("https://www.securityindustry.org/wp-content/uploads/sites/3/2018/05/noimage.png");
+  const handleSelected = async (base64: string) => {
+    console.log("Select "+ base64);
+    //const imgName = await http.post<string>("upload", {base64: base64});
+    //console.log(imgName.data);
+    
+    //setImages([...images,urlBackend+"files/"+imgName.data]);
+  };
 
-    const handleShow = async () => {
-        await setVisible(true);
-        let cropper = cropperObj;
-        if (!cropperObj) {
-          cropper = new Cropper(imgRef.current as HTMLImageElement, {
-            aspectRatio: 1 / 1,
-            viewMode: 1,
-            preview: prevRef.current as HTMLImageElement,
-          });
-        }
-        cropper?.replace("https://vovalohika.tk/images/1200_431btv0l.ykj.jpeg");
-        setCropperObj(cropper);
-    }
-
-    const handleCropped = async () => {
-        const base64 = cropperObj?.getCroppedCanvas().toDataURL() as string;
-        console.log("base64", base64);
-        setVisible(false);
-    }
 
     return (
       <>
         <h1>Головна сторінка</h1>
-        {/* <Button type="primary" onClick={handleShow}>
-          Завантажити фото
-        </Button> */}
-        <div>
-          <label htmlFor="uploadfile">
-            <img src={imageView} alt="фото" width="250" />
-          </label>
-          <input type="file" id="uploadfile" style={{display: "none"}} />
-        </div>
+        <Row gutter={[8, 8]}>
 
-        <Modal
-          title="Вибір фото"
-          centered
-          visible={visible}
-          onOk={handleCropped}
-          onCancel={() => setVisible(false)}
-          width={1000}
-          maskClosable={false}
-        >
-          <Row gutter={[8, 0]}>
-            <Col md={18} xs={24}>
-              <div>
-                <img
-                  ref={imgRef}
-                  src="https://vovalohika.tk/images/1200_431btv0l.ykj.jpeg"
-                  width="100%"
-                />
-              </div>
-            </Col>
-            <Col md={6} xs={24}>
-              <div
-                ref={prevRef}
-                style={{
-                  height: "100px",
-                  border: "1px solid silver",
-                  overflow: "hidden",
-                }}
-              ></div>
-            </Col>
-          </Row>
-        </Modal>
+        <Col md={4}>
+          <div>
+            <CropperModal onSelected={handleSelected} aspectRation={16 / 9} />
+          </div>
+        </Col>
+      </Row>
+        
+
       </>
     );
 }
---
+++ my-app/src/components/common/CropperModal/types.ts
@@ -0,0 +1,5 @@
+export interface ICropperProps {
+    onSelected: (base64: string) => void;
+    aspectRation?: number;
+  }
+  
